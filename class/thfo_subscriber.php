2311d41d56e5fa6ec5e29e3bf893ccbdca2bddc5
sebastienserre <sebastien@thivinfo.com>
2016-05-27 19:14:53 +0200

    improve subscribers list

--- class/thfo_subscriber.php
+++ class/thfo_subscriber.php
@@ -21,19 +21,44 @@ class thfo_subscriber
         $row=$wpdb->get_row("SELECT email FROM {$wpdb->prefix}thfo_newsletter_email");
         if (is_null($row)){
             echo __('No Subscriber at the moment', 'wp-post-updated');;
-        }else{
-            $data = $wpdb->get_results("SELECT email FROM {$wpdb->prefix}thfo_newsletter_email", ARRAY_A);
+        }
+
+        if (!is_null($row)){
+            $data = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}thfo_newsletter_email", ARRAY_A);
+	        var_dump($data);
             ?>
             <h2><?php _e('Subscribers list','wp-post-updated'); ?></h2>
-                <ul>
-                <?php
-                foreach ($data as $datas){
-                    ?>
-                    <li><?php echo $datas['email']; ?></li>
-                    <?php
-                }
-            ?>
-            </ul>
+	        <table>
+		        <tr>
+			        <th><?php _e('Email', 'wp-post-updated') ?></th>
+			        <th><?php _e('Post', 'wp-post-updated') ?></th>
+			        <th><?php _e('Delete', 'wp-post-updated') ?></th>
+		        </tr>
+	        <?php foreach ($data as $datas){
+		        $url = get_the_permalink($datas['post_id']);
+		        if ($datas['post_id'] == 0){
+			        $postname = __('All post', 'wp-post-updated');
+			        $url = '#';
+		        } else {
+			        $postname = get_the_title( $datas['post_id'] );
+		        }
+
+		        ?>
+		        <tr>
+			        <td><?php echo $datas['email'] ?></td>
+			        <td><a href="<?php echo $url; ?>"> <?php echo $postname; ?></a></td>
+			        <td>
+				        <?php
+				        $id = $datas['id'];
+				        $url = admin_url( 'admin.php?page=' );
+				        $url .= basename(dirname( __DIR__));
+				        $url .= '&id='. $id .'&delete=yes';
+				        ?>
+				        <a href="<?php echo esc_url($url); ?>" title="<?php _e('Delete', 'thfo-mail-alert') ?>"><span class="dashicons dashicons-trash"></span> </a> </td>
+		        </tr>
+	        <?php } ?>
+	        </table>
+
         <?php }
     }
 
